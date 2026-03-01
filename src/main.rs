a5e1b8fbdebf0504f6e878a4be6f68bf7ce91610
Polygons1 <47899327+Polygons1@users.noreply.github.com>
2026-03-01 23:39:09 +0200

    fixed compiler optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 use std::{hint::black_box, time::Instant};
 
 macro_rules! bench {
-    ($t:tt) => {
-        black_box({
-            let now = Instant::now();
-            let mut count = $t::MIN;
-            while count < $t::MAX {
-                count += 1;
-            }
-            assert_eq!(count, $t::MAX, "\x1b[31m\u{10102}\x1b[0m test failed");
-            let elapsed = now.elapsed();
-            println!(
-                "\x1b[32m\u{2714}\x1b[0m it took {}s to count to {} from {} ({})",
-                elapsed.as_secs_f64(),
-                $t::MAX,
-                $t::MIN,
-                stringify!($t)
-            )
-        })
-    };
+    ($t:tt) => {{
+        let now = Instant::now();
+        let mut count = $t::MIN;
+        while count < $t::MAX {
+            count = black_box(count + 1);
+        }
+        assert_eq!(count, $t::MAX, "\x1b[31m\u{10102}\x1b[0m test failed");
+        let elapsed = now.elapsed();
+        println!(
+            "\x1b[32m\u{2714}\x1b[0m it took {}s to count to {} from {} ({})",
+            elapsed.as_secs_f64(),
+            $t::MAX,
+            $t::MIN,
+            stringify!($t)
+        )
+    }};
 }
 
 fn main() {
